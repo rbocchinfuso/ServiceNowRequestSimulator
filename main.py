9ab28d59a66110a26aadc2ae0f18633e1de8e8fc
Richard Bocchinfuso <rbocchinfuso@gmail.com>
2020-06-10 21:06:28 +0000

    cleanup formatting

--- main.py
+++ main.py
@@ -16,24 +16,30 @@ from colorama import Fore, Back, Style
 from texttable import Texttable
 from tqdm import tqdm
 
+
 def temp_check():
     low_temp = config['temp']['low_temp']
     high_temp = config['temp']['high_temp']
-    temp_fahrenheit = float(decimal.Decimal(random.randrange(int(low_temp)*100, int(high_temp)*100))/100)
+    temp_fahrenheit = float(
+        decimal.Decimal(
+            random.randrange(int(low_temp) * 100,
+                             int(high_temp) * 100)) / 100)
     if temp_fahrenheit > 99.5:
         temp_result = 'no'
     elif temp_fahrenheit < 99.5:
         temp_result = 'yes'
     else:
         temp_result = 'na'
-    return [temp_fahrenheit, temp_result];
+    return [temp_fahrenheit, temp_result]
 
-def access_check(temp,ppe):
-    if ppe == 'yes' and temp =='yes':
+
+def access_check(temp, ppe):
+    if ppe == 'yes' and temp == 'yes':
         access_result = 'true'
     else:
-        access_result ='false'
-    return [access_result];
+        access_result = 'false'
+    return [access_result]
+
 
 def now(requester, location, temp_fahrenheit, temp_result, ppe_result, access):
     # define a resource
@@ -50,7 +56,8 @@ def now(requester, location, temp_fahrenheit, temp_result, ppe_result, access):
     # create a new sevicenow request record
     # result = request.create(payload=new_record)
     print(Fore.GREEN + '\nJSON payload sent to ServiceNow' + Style.RESET_ALL)
-    print (new_record)
+    print(new_record)
+
 
 def main():
     # locations
@@ -64,20 +71,23 @@ def main():
         location = random.choice(locations)
         temp = temp_check()
         ppe = random.choice(status)
-        access = access_check(temp[1],ppe)
+        access = access_check(temp[1], ppe)
         print(Fore.YELLOW + '\nThermalNet Simulator Data' + Style.RESET_ALL)
         t = Texttable()
-        t.set_cols_width([20,10,8,8,8,8])
-        t.set_cols_align(['c','c','c','c','c','c'])
-        t.add_rows([['requester', 'location', 'temp', 'temp ok', 'ppe ok', 'access'], [requester, location, temp[0], temp[1], ppe, access[0]]])
+        t.set_cols_width([20, 10, 8, 8, 8, 8])
+        t.set_cols_align(['c', 'c', 'c', 'c', 'c', 'c'])
+        t.add_rows(
+            [['requester', 'location', 'temp', 'temp ok', 'ppe ok', 'access'],
+             [requester, location, temp[0], temp[1], ppe, access[0]]])
         print(t.draw())
         now(requester, location, temp[0], temp[1], ppe, access[0])
         # time.sleep(cycle_time)
-        print ('\n')                
+        print('\n')
         # visual cycle time
         for i in tqdm(range(cycle_time)):
             time.sleep(1)
         count = count + 1
+
 
 if __name__ == '__main__':
     count = sys.argv[0]
@@ -86,7 +96,10 @@ if __name__ == '__main__':
     config.read('config.ini')
     config.sections()
     # servicenow client connection
-    c = pysnow.Client(instance=(config['now_api']['instance']), user=(config['now_api']['user']), password=(config['now_api']['password']))
+    c = pysnow.Client(
+        instance=(config['now_api']['instance']),
+        user=(config['now_api']['user']),
+        password=(config['now_api']['password']))
     # set cycle time
     cycle_time = int(config['local']['cycle_time'])
     main()
